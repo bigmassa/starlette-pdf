53e898b5b86709c4a1f761abb3342536518c5b06
Stuart George <stuart@accentdesign.co.uk>
2020-04-15 07:55:41 +0100

    use prebuild docker files

--- Dockerfile
+++ Dockerfile
@@ -1,63 +1,12 @@
-FROM        python:3.7-alpine as base
-
-RUN         set -ex \
-            && apk update \
-            && apk add --no-cache \
-                cairo-dev \
-                gdk-pixbuf-dev \
-                jpeg-dev \
-                libffi-dev \
-                make \
-                pango-dev \
-                ttf-freefont \
-                zlib-dev
-
-# install package requirements into a copy of the base image
-# this will include a butt load of cache
-
-FROM        base as builder
-
+ARG         EXTRA_DEPS="cairo-dev gdk-pixbuf-dev libffi-dev make pango-dev ttf-freefont"
 ARG         REQUIREMENTS_FILE=/requirements/base.txt
 
-RUN         mkdir /install
+FROM        accent/starlette-docker:3.8-alpine
 
-WORKDIR     /install
+ENV         APP_MODULE=app.main:app \
+            ALLOWED_HOSTS="*" \
+            SECRET_KEY="***** change me *****"
 
-COPY        requirements /requirements
-
-RUN         set -ex \
-            && apk update \
-            && apk add --no-cache --virtual .build-deps \
-                gcc \
-                git \
-                libc-dev \
-                linux-headers \
-                musl-dev \
-            && PYTHONUSERBASE=/install pip install --user --no-warn-script-location -r $REQUIREMENTS_FILE \
-            && apk del .build-deps
-
-# get a fresh copy of the base image and copy in packages without all the cache
-
-FROM        base
-
-COPY        --from=builder /install /usr/local
-
-COPY        ./src /app
-
-COPY        start.sh /start.sh
-COPY        start-reload.sh /start-reload.sh
-COPY        gunicorn_conf.py /gunicorn_conf.py
-
-WORKDIR     /app
-
-# Django configuration:
-
-ENV         PYTHONUNBUFFERED=1 \
-            PYTHONPATH=/app \
-            ALLOWED_HOSTS="*"
-
-# Entry:
-
-EXPOSE      80
+ENTRYPOINT  []
 
 CMD         ["/start.sh"]
--- src/app/main.py
+++ src/app/main.py
@@ -1,29 +1,43 @@
 from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.gzip import GZipMiddleware
 from starlette.middleware.trustedhost import TrustedHostMiddleware
+from starlette.routing import Mount, Route
 from starlette.staticfiles import StaticFiles
 
-from app import endpoints, handlers, settings
+from app import endpoints, globals, handlers, settings
 
-# base app
-app = Starlette(debug=settings.DEBUG)
+static = StaticFiles(directory="static")
+staticapp = GZipMiddleware(static)
 
-# routes
-app.add_route("/", endpoints.Home, methods=["GET"], name="home")
-app.add_route(
-    "/from-string", endpoints.FromString, methods=["GET", "POST"], name="from-string"
+routes = [
+    Route("/", endpoints.Home, methods=["GET"], name="home"),
+    Route(
+        "/from-string",
+        endpoints.FromString,
+        methods=["GET", "POST"],
+        name="from-string",
+    ),
+    Route("/from-url", endpoints.FromURL, methods=["GET", "POST"], name="from-url"),
+    Mount("/static", app=staticapp, name="static"),
+]
+
+middleware = [
+    Middleware(TrustedHostMiddleware, allowed_hosts=settings.ALLOWED_HOSTS),
+]
+
+exception_handlers = {
+    404: handlers.not_found,
+    500: handlers.server_error,
+}
+
+app = Starlette(
+    debug=settings.DEBUG,
+    routes=routes,
+    middleware=middleware,
+    exception_handlers=exception_handlers,  # type: ignore
 )
-app.add_route("/from-url", endpoints.FromURL, methods=["GET", "POST"], name="from-url")
 
-app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
-
-# middleware
-app.add_middleware(TrustedHostMiddleware, allowed_hosts=settings.ALLOWED_HOSTS)
-
-# exception handlers
-app.add_exception_handler(404, handlers.not_found)
-app.add_exception_handler(500, handlers.server_error)
-
-# sentry
 if settings.SENTRY_DSN:
     try:
         from sentry_asgi import SentryMiddleware
